051eea4e7d55309b329d68dd1e1b6268584d019a
Lautaro Paskevicius <lautaro.paskevicius@ing.austral.edu.ar>
2017-03-19 23:07:53 -0300

    (TP 01) - 6 added, finished, tested (recursiveGetPrimeFactors missing)

---
+++ src/Tp1/Ej6.java
@@ -0,0 +1,116 @@
+package Tp1;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Lautaro Paskevicius (lautaro.paskevicius@ing.austral.edu.ar)
+ * on 19/03/2017.
+ */
+public class Ej6 {
+
+    public boolean isPrime(Integer n){
+
+        boolean answer = true;
+
+        if (n == 2 || n == 3){
+            return  true;
+        }
+
+        try{
+            if(n < 0 || n ==1) throw new RuntimeException();
+        } catch (RuntimeException e){
+            System.out.println("n must be > 0 or != 1!");
+        }
+
+        Double dLimit = Math.floor(Math.sqrt(n));
+        Integer limit =  dLimit.intValue();
+
+        for(int i = 2; i <= limit; i++){
+            if (n % i == 0){
+                answer = false;
+                break;
+            }
+        }
+
+        return answer;
+    }
+
+    public boolean recursiveIsPrime(int n) {
+        boolean answer = true;
+
+        if (n == 2 || n == 3){
+            return  true;
+        }
+
+        try{
+            if(n < 0 || n ==1) throw new RuntimeException();
+        } catch (RuntimeException e){
+            System.out.println("n must be > 0 or != 1!");
+        }
+
+        Double dLimit = Math.floor(Math.sqrt(n));
+        Integer limit =  dLimit.intValue();
+
+        answer = recursiveIsPrime(n, 2, limit);
+
+        return answer;
+    }
+
+    private boolean recursiveIsPrime(int n, int floor, int limit){
+        if (n % floor == 0 && n != floor){
+            return false;
+        } else if ( floor > n){
+            return true;
+        }
+
+        floor++;
+
+        return recursiveIsPrime(n, floor, limit);
+    }
+
+    public int firstBiggerPrime(int n){
+
+        while (!isPrime(n)){
+            n = n + 1;
+        }
+
+        return n;
+    }
+
+    public int recursiveBiggerPrime(int n){
+        if(isPrime(n)) return n;
+        return recursiveBiggerPrime(++n);
+    }
+
+    public int numberOfPrimeFactors(int n){
+        return primeFactors(n).size();
+    }
+
+    public  ArrayList<Integer> primeFactors(int n) {
+        ArrayList<Integer> factors = new ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        return factors;
+    }
+
+   /* public ArrayList<Integer> recursivePrimeFactors(int n){
+        ArrayList<Integer> factors = new ArrayList<>();
+
+        return recursivePrimeFactors(n,2,factors);
+    }
+
+    private ArrayList<Integer> recursivePrimeFactors(int n,int i,ArrayList<Integer> factors) {
+        if(n <= i) return factors;
+
+        while(n % i == 0){
+            factors.add(i);
+            n /= i;
+        }
+
+        return recursivePrimeFactors(n,++i,factors);
+    }*/
+}
---
+++ test/Tp1/Ej6Test.java
@@ -0,0 +1,69 @@
+package Tp1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lautaro Paskevicius (lautaro.paskevicius@ing.austral.edu.ar)
+ * on 19/03/2017.
+ */
+public class Ej6Test {
+
+    Ej6 ej6 = new Ej6();
+    int[] primes = {2,3,5,7,11,13,17,19};
+    int[] notPrimes = {4,6,8,9,10,12,14,15,16};
+
+    @Test
+    public void isPrime() throws Exception {
+        for (int e : primes){
+            assertEquals(true,ej6.isPrime(e));
+        }
+
+        for (int e : notPrimes){
+            assertEquals(false,ej6.isPrime(e));
+        }
+    }
+
+    @Test
+    public void recursiveIsPrime() throws Exception {
+
+
+        for (int e : primes){
+            assertEquals(true,ej6.recursiveIsPrime(e));
+        }
+
+        for (int e : notPrimes){
+            assertEquals(false,ej6.recursiveIsPrime(e));
+        }
+    }
+
+    @Test
+    public void firstBiggerPrime() throws Exception {
+        assertEquals(11,ej6.firstBiggerPrime(8));
+        assertEquals(11,ej6.firstBiggerPrime(11));
+    }
+
+    @Test
+    public void recursiveBiggerPrime() throws Exception {
+        assertEquals(11,ej6.recursiveBiggerPrime(8));
+        assertEquals(11,ej6.recursiveBiggerPrime(11));
+    }
+
+    @Test
+    public void numberOfPrimeFactors() throws Exception {
+        assertEquals(2,ej6.numberOfPrimeFactors(15));
+        assertEquals(3,ej6.numberOfPrimeFactors(8));
+    }
+
+    @Test
+    public void getPrimeFactors() throws  Exception {
+        System.out.println(ej6.primeFactors(15).toString());
+        System.out.println(ej6.primeFactors(8).toString());
+    }
+
+    /*@Test
+    public void recursivePrimeFactors() throws Exception {
+        System.out.println(ej6.recursivePrimeFactors(15).toString());
+        System.out.println(ej6.recursivePrimeFactors(8).toString());
+    }*/
+}
