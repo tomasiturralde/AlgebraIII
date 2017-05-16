3d2fa3e64526563e0a9f646f85f727231c22caf8
Tomas <tomas.iturralde@ing.austral.edu.ar>
2017-05-16 13:24:43 -0300

    añadida impl de calculator y modif ejercicio 1 para cumplir requisitos

---
+++ src/Tp6/CalculatoImpl.java
@@ -0,0 +1,49 @@
+package Tp6;
+
+/**
+ * Created by Tomas on 16/5/2017.
+ */
+public class CalculatoImpl implements Calculator{
+    private int sum, subtraction, multiplication, division;
+
+    public CalculatoImpl(){
+        sum = subtraction = multiplication = division = 0;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+    public int getSubtraction() {
+        return subtraction;
+    }
+    public int getMultiplication() {
+        return multiplication;
+    }
+    public int getDivision() {
+        return division;
+    }
+
+    @Override
+    public double sum(double a, double b) {
+        sum++;
+        return a+b;
+    }
+
+    @Override
+    public double subtraction(double a, double b) {
+        subtraction++;
+        return a-b;
+    }
+
+    @Override
+    public double multiplication(double a, double b) {
+        multiplication++;
+        return a*b;
+    }
+
+    @Override
+    public double division(double a, double b) {
+        division++;
+        return a/b;
+    }
+}
