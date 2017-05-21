ca6e880f6aa0dc724cdf552879acd7b0d5a753cb
Tomas <tomas.iturralde@ing.austral.edu.ar>
2017-05-21 17:57:43 -0300

    ej3 parte e done

---
+++ src/Tp6/Ej4.java
@@ -0,0 +1,15 @@
+package Tp6;
+
+import Tp6.Interfaces.Calculator;
+import Tp6.Interfaces.Exercise4;
+
+/**
+ * Created by Tomas on 21/5/2017.
+ */
+public class Ej4 implements Exercise4{
+
+    @Override
+    public double[][] summation(double[] matrixA, double[] matrixB, Calculator calculator) {
+        return new double[0][];
+    }
+}
