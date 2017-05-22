baedc61b2af5f309cdaf9cc2ca4ab08385a10197
Tomas <tomas.iturralde@ing.austral.edu.ar>
2017-05-21 22:32:39 -0300

    ej4 partially done

--- src/Tp6/Ej4.java
+++ src/Tp6/Ej4.java
@@ -10,6 +10,20 @@ public class Ej4 implements Exercise4{
 
     @Override
     public double[][] summation(double[] matrixA, double[] matrixB, Calculator calculator) {
-        return new double[0][];
+        if (matrixA.length == matrixB.length) {
+            double[] summation = new double[matrixA.length];
+            for (int i = 0; i < matrixA.length; i++) {
+                summation[i] = calculator.sum(matrixA[i], matrixB[i]);
+            }
+
+            double[][] result = new double[dimensionFinder(matrixA.length)][dimensionFinder(matrixB.length)];
+
+            return result;
+        }
+        throw new RuntimeException("Both vectors must have the same length");
+    }
+
+    private int dimensionFinder(int n){
+        return (int)((-1 + Math.sqrt(1 + 8 * n)) / 2);
     }
 }
