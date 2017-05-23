53d0eb5dbb2a36f572b48c41f602e164e60c6906
Tomas <tomas.iturralde@ing.austral.edu.ar>
2017-05-23 11:07:12 -0300

    added tp7

---
+++ src/Tp7/Exercises.java
@@ -0,0 +1,46 @@
+package Tp7;
+
+/**
+ * Created by Tomas on 23/5/2017.
+ */
+public class Exercises implements TP4 {
+    @Override
+    public double[] exercise1(double[][] coefficients, double[] independentTerms) {
+        return new double[0];
+    }
+
+    @Override
+    public double[] exercise2(double[][] coefficients, double[] independentTerms) {
+        return new double[0];
+    }
+
+    @Override
+    public double[] exercise5WithoutPivoteo(double[][] coefficients, double[] independentTerms) {
+        return new double[0];
+    }
+
+    @Override
+    public double[] exercise5PartialPivoteo(double[][] coefficients, double[] independentTerms) {
+        return new double[0];
+    }
+
+    @Override
+    public double[] exercise6(double[][] coefficients, double[] independentTerms, Calculator calculator) {
+        return new double[0];
+    }
+
+    @Override
+    public double[] exercise7(double[][] coefficients, double[] independentTerms, Calculator calculator) {
+        return new double[0];
+    }
+
+    @Override
+    public double[][] exercise8(double[][] coefficients) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[] exercise9(double[][] coefficients, double[] independentTerms) {
+        return new double[0];
+    }
+}
